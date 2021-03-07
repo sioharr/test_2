cb923d646f66b046247b435e1e62f915eb252def
sioharr <31242932+sioharr@users.noreply.github.com>
2021-03-06 18:20:42 -0700

    Add files via upload

---
+++ autograde_ps2_1.R
@@ -0,0 +1,10 @@
+cur.name <- "What is the p value?"
+cur.output <-"Correct" 
+score <- 2
+max.score <- 1
+    
+    
+cat(sprintf("Test %s:\n", cur.name ))
+cat(sprintf("Output:\n %s\n", cur.output))
+cat(sprintf("Score: %g/%g\n", score, max.score))
+cat("====================================================\n\n\n")
